4ef73d9522ca335310a089f51e9696c989c736c8
frankdonny <arkohfrank1@gmail.com>
2023-03-03 01:50:54 +0000

    0x00. Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,11 +19,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields) -> None:
+        """initializing fields"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """format the record"""
-        message = super().format(record)
+        message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
