a3aee3f6935102fb43781ffe835d79e880ae723c
frankdonny <arkohfrank1@gmail.com>
2023-03-07 03:01:51 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,20 +18,20 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> tuple:
+def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple:
+def unauthorized(error) -> str:
     """unathorized access"""
     return jsonify({'error': 'Unauthorized'}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> tuple:
+def forbidden(error) -> str:
     """unathorized access"""
     return jsonify({'error': 'Forbidden'}), 403
 
