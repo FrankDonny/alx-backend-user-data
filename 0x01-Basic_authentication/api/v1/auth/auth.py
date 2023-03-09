c241825d3e22ca161014992fc7df80af37f763cc
frankdonny <arkohfrank1@gmail.com>
2023-03-09 23:33:47 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -32,8 +32,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """current user method"""
         return None
-
-
-class BasicAuth(Auth):
-    """the Basic Auth class"""
-    pass
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module containing the basic authentication class"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """the Basic Auth class"""
+    pass
