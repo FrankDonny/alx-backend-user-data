11c7f96d969996cc0221ec928c62a62f2e184a11
frankdonny <arkohfrank1@gmail.com>
2023-03-09 16:25:13 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,9 @@ def stats() -> str:
 def unauthorized():
     """unauthorized route"""
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """forbidden route"""
+    abort(403)
