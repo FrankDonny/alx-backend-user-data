39006147020db2e363ee1c573416d7e25896cae7
frankdonny <arkohfrank1@gmail.com>
2023-03-10 00:24:02 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,10 @@
 """module containing the basic authentication class"""
 import base64
 
-from api.v1.auth.auth import Auth
+# from api.v1.auth.auth import Auth
 
 
-class BasicAuth(Auth):
+class BasicAuth():
     """the Basic Auth class"""
 
     def extract_base64_authorization_header(self,
@@ -33,3 +33,26 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> (str, str):
+        """extract user credentials"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        else:
+            db64 = decoded_base64_authorization_header.split(':')
+            return (db64[0], db64[1])
+
+
+a = BasicAuth()
+
+print(a.extract_user_credentials(None))
+print(a.extract_user_credentials(89))
+print(a.extract_user_credentials("Holberton School"))
+print(a.extract_user_credentials("Holberton:School"))
+print(a.extract_user_credentials("bob@gmail.com:toto1234"))
