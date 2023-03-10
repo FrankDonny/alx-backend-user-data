bcc87f4d6153fa701c2d2de744d770b504c43d32
frankdonny <arkohfrank1@gmail.com>
2023-03-10 00:12:19 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """module containing the basic authentication class"""
+import base64
+
 from api.v1.auth.auth import Auth
 
 
@@ -17,3 +19,16 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except base64.binascii.Error:
+            return None
